fe6c834af6b36feea356acd9003a572330a45da5
Kim <kim.russel@yandex.ru>
2021-01-10 10:25:05 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
 import useFetchJobs from './useFetchJobs'
 import { Container } from 'react-bootstrap'
+import Job from './Job'
+
 function App() {
   const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
   const { jobs, loading, error } = useFetchJobs(params, page)
 
   return (
     <Container>
       {loading && <h1>loading..</h1> }
-      {error && <h1>Error. Try refreshing.</h1> }
-      <h1>{jobs.length}</h1>
+      {error && <h1>Error{error.e} Try refreshing.</h1> }
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
     </Container>
   );
 }
--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -27,17 +27,19 @@ export default function useFetchJobs(params, page) {
 	const [state, dispatch] = useReducer(reducer, {jobs: [], loading: true})
 	
     useEffect(() =>{
-      coonst cancelToken = axios.cancelToken.source()
+
+      const cancelToken = axios.CancelToken.source()
+      
       dispatch({ type: ACTIONS.MAKE_REQUEST})
       axios.get(BASE_URL, {
       	cancelToken: cancelToken.token,
       	params: { markdown: true, page: page, ...params }
       }).then(res => {
       	dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } })
-        }).catch(e => {
-          if (axios.isCancel(e)) return
+      }).catch(e => {
+        if (axios.isCancel(e)) return
         dispatch({ type: ACTIONS.ERROR, payload: { error: e } })	
-        })
+      })
 
         return () => {
         	cancelToken.cancel()
